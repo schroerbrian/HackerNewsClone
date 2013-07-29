67f6661ae1ba240eba7748aef0442b2705ae8c19
Brian Schroer <schroer.brian@gmail.com>
2013-07-29 14:47:07 -0700

    initial commit

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :text
+
+  belongs_to :link
+
+  validates_presence_of :text, :link
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNews::Application.config.secret_token = '7cd1396d244a753c6bcf9bb2eb7ea8b058c5a8498de14dba8d5a5cea3249e9ce812c7938f9363fc40c92106a24728674bd1293b161f43361e37349b586c26846'
